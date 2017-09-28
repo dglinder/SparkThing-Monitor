9e970faafcb929f9000131ae4d880fdc7c66ff34
Daniel Linder <dan@linder.org>
2017-09-27 21:18:42 -0500

    Minor update to check Git commits.

--- creds.h
+++
@@ -1,9 +0,0 @@
-// WiFi network info.
-char ssid[] = "xx";
-char wifiPassword[] = "xx";
-
-// Cayenne authentication info. This should be obtained from the Cayenne Dashboard.
-char username[] = "xx";
-char password[] = "xx";
-char clientID[] = "xx";
-
