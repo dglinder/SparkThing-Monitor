e4b61b53f3b35b531532b3c95c791706ff72f0ca
Daniel Linder <dan@linder.org>
2017-10-10 12:11:37 -0500

    Create creds.h
    Add sample creds.h file.

---
+++ creds.h
@@ -0,0 +1,7 @@
+// A: WiFi network info.
+char ssid[] = "xx";
+char wifiPassword[] = "xx";
+// B: Cayenne authentication info. This should be obtained from the Cayenne Dashboard.
+char username[] = "xx";
+char password[] = "xx";
+char clientID[] = "xx";
